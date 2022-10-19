e67381a567cb65b4d9febe16bec534a76bf5b53e
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-10-19 20:22:29 +0300

    updated configuration server

--- config-server/src/test/java/corp/siendev/com/keeper/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/corp/siendev/com/keeper/configserver/ConfigServerApplicationTests.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2020
- * Ghost Rider aka Golubnichenko Iurii
+ * Copyright (c) 2022
+ * Ghost Rider aka Iurii Golubnichenko
  */
 
 package corp.siendev.com.keeper.configserver;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ConfigServerApplicationTests {
-
     @Test
     public void contextLoads() {
     }
-
 }
